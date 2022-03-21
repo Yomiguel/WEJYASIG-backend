afb11dec0416c06f53efab6ec921d31c19c04858
Luis <yomiguel112@gmail.com>
2022-03-21 10:12:23 -0500

    output data sorted

--- controllers/stationsController.js
+++ controllers/stationsController.js
@@ -3,7 +3,7 @@ const { calculateAQI, calculateConcentration, getColor } = require("../aqi");
 const stationsIds = require("../data/stationsId.json");
 
 const formatStationsData = (stations) => {
-  const filteredStationsData = stations.filter(
+  const filteredStations = stations.filter(
     (station) =>
       station.id == stationsIds.stations[0].id ||
       station.id == stationsIds.stations[1].id ||
@@ -12,12 +12,22 @@ const formatStationsData = (stations) => {
       station.id == stationsIds.stations[4].id
   );
 
-  const formattedStationsData = filteredStationsData.map(({ measurements }) => {
-    const pm25 = measurements
-    console.log(pm25);
-     return pm25;
+  const filteredStationsData = filteredStations.map(({ measurements }) => {
+    const stationsData = measurements[0][1];
+    return stationsData;
   });
 
+  const formattedStationsData = filteredStationsData.map(({ measurementDeterminedBy, measurementValue }) => {
+    const pm25Value = measurementValue;
+    const stationsId = measurementDeterminedBy;
+    return {
+      "pm2.5": pm25Value,
+      "aqi": Number(calculateAQI(pm25Value).toFixed(0)),
+      "station": stationsId 
+
+    };
+  });
+  console.log(formattedStationsData);
   return formattedStationsData;
 };
 
