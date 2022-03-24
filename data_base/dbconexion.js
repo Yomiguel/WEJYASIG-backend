f0c1a73828484ebaa65c428b4ec9af142d829833
Luis <yomiguel112@gmail.com>
2022-03-24 10:26:56 -0500

    db URL

--- data_base/dbconexion.js
+++ data_base/dbconexion.js
@@ -1,6 +1,6 @@
 const pgp = require("pg-promise")();
 const db = pgp(
-  "postgres://postgres:postgres@database-1.cj189klt7leg.us-east-1.rds.amazonaws.com:5432/proyecto"
+  
 );
 
 module.exports = db;
