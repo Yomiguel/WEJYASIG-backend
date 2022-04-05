959faf50314fae964fd6dc330b3d9e59b7cc4632
Luis <yomiguel112@gmail.com>
2022-04-05 17:38:52 -0500

    deleted credentials

--- data_base/dbconexion.js
+++ data_base/dbconexion.js
@@ -5,7 +5,3 @@ require("dotenv").config();
 const db = pgp(`postgres://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:5432/${process.env.DATABASE}`);
 
 module.exports = db;
-
-
-/*postgres://postgres:postgres@database-1.cj189klt7leg.us-east-1.rds.amazonaws.com:5432/proyecto*/
-/*`postgres://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:5432/${process.env.DATABASE}`*/
